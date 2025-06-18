a4e29f8f4d822cd09080da19cc8295f8c735da56
AgungDH <agungdh@live.com>
2025-06-18 11:15:45 +0700

    add Tag entity, repository, and HelloController; update MariaDB configuration

---
+++ src/main/java/my/id/agungdh/tagservice/repository/TagRepository.java
@@ -0,0 +1,6 @@
+package my.id.agungdh.tagservice.repository;
+
+import my.id.agungdh.tagservice.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {}
