95c6144e6edc3b5a1100a1ca142147d1e51224ad
AgungDH <agungdh@live.com>
2025-06-18 19:34:33 +0700

    refactor TagRepository interface and fix XML namespace order in pom.xml

--- src/main/java/my/id/agungdh/tagservice/repository/TagRepository.java
+++ src/main/java/my/id/agungdh/tagservice/repository/TagRepository.java
@@ -3,4 +3,5 @@ package my.id.agungdh.tagservice.repository;
 import my.id.agungdh.tagservice.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TagRepository extends JpaRepository<Tag, Long> {}
+public interface TagRepository extends JpaRepository<Tag, Long> {
+}
